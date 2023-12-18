ab107886cd9a9d8b651a5f64e8e8db899b6fb854
itsmejatins <itsmejatin.s@gmail.com>
2023-12-18 19:03:18 +0530

    Fixed and modified Dsu template, added nComponents and Map of components feature

--- src/templates/dataStructures/DSU.java
+++ src/templates/dataStructures/DSU.java
@@ -1,12 +1,17 @@
 package templates.dataStructures;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-public class DSU
+public class Dsu
 {
 	int parent[], rank[];
+	int nComponents = 0;
+	Map<Integer, Integer> components; // (id, size)
+	boolean wantMap = false;
 
-	public DSU(int n)
+	public Dsu(int n)
 	{
 		parent = new int[n];
 		Arrays.fill(parent, -1);
@@ -14,10 +19,25 @@ public class DSU
 		Arrays.fill(rank, -1);
 	}
 
+	public Dsu(int n, boolean wantMap)
+	{
+		this(n);
+		if (wantMap)
+		{
+			components = new HashMap<>();
+			this.wantMap = true;
+		}
+	}
+
 	public void createSet(int i)
 	{
 		parent[i] = i;
 		rank[i] = 0;
+		nComponents++;
+		if (wantMap)
+		{
+			components.put(i, 1);
+		}
 	}
 
 	public int getParent(int i)
@@ -36,21 +56,33 @@ public class DSU
 
 		if (pX == pY)
 			return;
-		
-//		if (rank[pX] > rank[pY])
-//		{
-//			parent[pY] = pX;
-//		}
-//		else
-//		{
-//			parent[pX] = pY;
-//			if (rank[pX] == rank[pY])
-//			{
-//				rank[pY]++;
-//			}
-//		}
-		
-		if(pX != pY)
+
+		if (rank[pX] > rank[pY])
+		{
+			if(wantMap)
+			{
+				int a = components.get(pX), b = components.get(pY);
+				components.put(pX, a + b);
+				components.remove(pY);
+			}
 			parent[pY] = pX;
+			nComponents--;
+		}
+		else
+		{
+			if(wantMap)
+			{
+				int a = components.get(pX), b = components.get(pY);
+				components.put(pY, a + b);
+				components.remove(pX);
+			}
+			parent[pX] = pY;
+			if (rank[pX] == rank[pY])
+			{
+				rank[pY]++;
+			}
+			nComponents--;
+		}
+
 	}
 }
